99807ef58acec6ec3c0646cc9587332131a5ce7c
hayat00444 <hayat0004444@gmail.com>
2026-02-08 20:40:26 +0530

    Update demo dialog content and adjust button styling

--- src/pages/Earn.tsx
+++ src/pages/Earn.tsx
@@ -24,14 +24,12 @@ const Earn = () => {
           <GameDialogContent title="Profile Info">
             <GameDialogBody>
               {/* Demo content */}
-              <div className="text-white space-y-4 px-3">
-             idiot i want content have same margin on both side
-              </div><div className="text-white space-y-4 px-3">
-             idiot i want content have same margin on both side
+              <div >
+                This is a demo dialog. You can put any content here, such as forms, information, or actions related to earning rewards in the game.
               </div>
             </GameDialogBody>
             <GameDialogFooter>
-              <GameButton variant="black" size="lg" className="flex-1" onClick={() => setOpen(false)}>
+              <GameButton variant="black" size="lg" className=" w-2 flex-1" onClick={() => setOpen(false)}>
                 Cancel
               </GameButton>
               <GameButton variant="gold" size="lg" className="flex-1" onClick={() => setOpen(false)}>
