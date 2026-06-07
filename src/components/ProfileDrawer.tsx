307c14de85810b00de38fe4a85da74cce6db4e32
hayat00444 <hayat0004444@gmail.com>
2026-06-07 17:12:53 +0530

    fix: add mr-3 to badge right side

--- src/components/ProfileDrawer.tsx
+++ src/components/ProfileDrawer.tsx
@@ -134,7 +134,7 @@ const ProfileDrawer = ({ open, onOpenChange }: ProfileDrawerProps) => {
             {vipData && (
               <button
                 onClick={() => { onOpenChange(false); navigateWithTransition("/vip"); }}
-                className="flex-shrink-0 cursor-pointer"
+                className="flex-shrink-0 cursor-pointer mr-3"
               >
                 <img src={getVipBadge(vipLevelIndex)} alt="VIP" className="w-[105px] h-[54px] object-contain" />
               </button>
