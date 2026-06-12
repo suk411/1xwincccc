5233612682e0c963667e3dcbc1d407a9b0a7b58b
hayat00444 <hayat0004444@gmail.com>
2026-06-12 21:45:02 +0530

    Match test.html UI: blue gradient text with white outline on PageHeader

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -39,12 +39,23 @@ const PageHeader = ({ title, showBack = true, backPath }: PageHeaderProps) => {
       )}
 
       {/* Title */}
-      <h1 className="relative z-10 tracking-wide" style={{
-        color: "#FFFFFF",
-        textShadow: "rgba(5, 16, 48, 0.7) -1.3px -1.3px 0px, rgba(5, 16, 48, 0.7) 1.3px -1.3px 0px, rgba(5, 16, 48, 0.7) -1.3px 1.3px 0px, rgba(5, 16, 48, 0.7) 1.3px 1.3px 0px",
-      }}>
-        {title}
-      </h1>
+      <div className="relative z-10">
+        <h1 className="tracking-wide absolute" style={{
+          color: "#FFFFFF",
+          top: "-0.7px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          whiteSpace: "nowrap",
+          textShadow: "rgba(5, 16, 48, 0.7) -1.3px -1.3px 0px, rgba(5, 16, 48, 0.7) 1.3px -1.3px 0px, rgba(5, 16, 48, 0.7) -1.3px 1.3px 0px, rgba(5, 16, 48, 0.7) 1.3px 1.3px 0px",
+        }}>{title}</h1>
+        <h1 className="tracking-wide" style={{
+          backgroundImage: "linear-gradient(0deg, rgb(70, 110, 208) 0%, rgb(64, 72, 179) 43.7%, rgb(97, 130, 237) 45%, rgb(101, 127, 231) 100%)",
+          WebkitBackgroundClip: "text",
+          backgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          color: "transparent",
+        }}>{title}</h1>
+      </div>
     </div>
   );
 };
