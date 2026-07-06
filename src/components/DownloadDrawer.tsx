ee7b02c35c813737bd6cf465051715f7dd3f8aab
hayat00444 <hayat0004444@gmail.com>
2026-07-06 10:04:50 +0530

    wrap svg+text in flex div for horizontal alignment

--- src/components/DownloadDrawer.tsx
+++ src/components/DownloadDrawer.tsx
@@ -114,23 +114,24 @@ const DownloadDrawer = ({ open, onOpenChange }: DownloadDrawerProps) => {
             onClick={handleInstall}
             style={{ width: "305px" }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="white"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              style={{ marginRight: "10px" }}
-            >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-              <polyline points="7 10 12 15 17 10" />
-              <line x1="12" y1="15" x2="12" y2="3" />
-            </svg>
-            Install APP
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                <polyline points="7 10 12 15 17 10" />
+                <line x1="12" y1="15" x2="12" y2="3" />
+              </svg>
+              Install APP
+            </div>
           </GameButton>
         </div>
       </DrawerContent>
